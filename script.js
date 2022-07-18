e2169c5501c53a309dcca3e2222d34bf00fe4925
Jason Cassella <jasoncassella1@gmail.com>
2022-07-18 00:24:10 -0400

    created Ship class

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const gameBoard = document.querySelector('#gameBoard');
 
-class GameBoard {
-  // call ship function to place ships at specific coordinates
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hitLocation = [];
+    this.sunkStatus = false;
+  }
 
+  hit(number) {
+    this.hitLocation.push(number);
+  }
 
-}
+  isSunk() {
+    if (this.length === this.hitLocation.length) {
+      return true;
+    }
+    else return false;
 
-const board = new GameBoard();
-
-
-function createBoard() {
-  for (i = 0; i < 64; i++) {
-    const square = document.createElement('div');
-    gameBoard.appendChild(square).className = 'square';
   }
 }
 
-createBoard();
+
+
+// class GameBoard {
+//   // call ship function to place ships at specific coordinates
+// }
+
+// const board = new GameBoard();
+
+
+// function createBoard() {
+//   for (i = 0; i < 64; i++) {
+//     const square = document.createElement('div');
+//     gameBoard.appendChild(square).className = 'square';
+//   }
+// }
+
+// createBoard();
+
+let carrier = new Ship(5);
+
+carrier.hit(1);
+carrier.hit(2);
+carrier.hit(3);
+carrier.hit(4);
+carrier.hit(5);
+
+console.log(carrier.isSunk());
